1622167ecca9ad442dfea5514726ab1a7b2be5c0
HugoSantos14 <hugosantos14000@gmail.com>
2025-11-11 23:45:22 -0300

    feat: add seed data for 'categorias' table

---
+++ dml/02_seed_categorias.sql
@@ -0,0 +1,11 @@
+INSERT INTO categorias (nome)
+VALUES ('Banco de Dados');
+
+INSERT INTO categorias (nome)
+VALUES ('HTML');
+
+INSERT INTO categorias (nome)
+VALUES ('Java');
+
+INSERT INTO categorias (nome)
+VALUES ('PHP');
