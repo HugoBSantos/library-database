b7febdfdc1cd5eb499fbc7e57bf5fabf014ccee1
HugoSantos14 <hugosantos14000@gmail.com>
2025-11-12 11:30:57 -0300

    feat: add seed data for 'alunos' table

---
+++ dml/06_seed_alunos.sql
@@ -0,0 +1,14 @@
+INSERT INTO alunos (nome)
+VALUES ('Mario');
+
+INSERT INTO alunos (nome)
+VALUES ('João');
+
+INSERT INTO alunos (nome)
+VALUES ('Paulo');
+
+INSERT INTO alunos (nome)
+VALUES ('Pedro');
+
+INSERT INTO alunos (nome)
+VALUES ('Maria');
