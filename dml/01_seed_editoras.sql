89e929edbf61272b77a7a484cd17e8ce299d6dda
HugoSantos14 <hugosantos14000@gmail.com>
2025-11-11 23:41:02 -0300

    feat: add seed data for 'editoras' table

---
+++ dml/01_seed_editoras.sql
@@ -0,0 +1,11 @@
+INSERT INTO editoras (nome)
+VALUES ('Bookman');
+
+INSERT INTO editoras (nome)
+VALUES ('Edgard Blusher');
+
+INSERT INTO editoras (nome)
+VALUES ('Nova Terra');
+
+INSERT INTO editoras (nome)
+VALUES ('Brasport');
